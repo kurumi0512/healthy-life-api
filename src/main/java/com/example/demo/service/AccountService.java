3cda0d39ce627e5ca50ee85f01a1886b987e73ba
kurumi0512 <foreverlove0512t@gmail.com>
2025-05-20 12:36:37 +0800

    250520 12:36

--- src/main/java/com/example/demo/service/AccountService.java
+++ src/main/java/com/example/demo/service/AccountService.java
@@ -1,38 +1,7 @@
 package com.example.demo.service;
 
-import java.time.LocalDateTime;
+public interface AccountService {
+	void register(String username, String password, String email);
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.example.demo.model.entity.Account;
-import com.example.demo.repository.AccountRepository;
-import com.example.demo.util.HashUtil;
-
-@Service
-public class AccountService {
-
-	@Autowired
-	private AccountRepository accountRepository;
-
-	public void register(String username, String password, String email) {
-		String salt = HashUtil.generateSalt();
-		String hashPassword = HashUtil.hashPassword(password, salt);
-
-		Account account = new Account();
-		account.setUsername(username);
-		account.setEmail(email);
-		account.setHashSalt(salt);
-		account.setHashPassword(hashPassword);
-		account.setCompleted(false);
-		account.setStatus("active");
-		account.setCreateTime(LocalDateTime.now());
-
-		accountRepository.save(account);
-	}
-
-	public boolean isUsernameTaken(String username) {
-		return accountRepository.findByUsername(username).isPresent();
-	}
-
+	boolean isUsernameTaken(String username);
 }
